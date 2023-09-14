c04c092a18be71ea2e95b40fca9335b40dd6a98f
RobbieRob2k <139776035+RobbieRob2k@users.noreply.github.com>
2023-09-14 14:51:44 -0300

    Update trying_v1.py

--- trying_v1.py
+++ trying_v1.py
@@ -6,7 +6,25 @@ st.title("This is a Test Website")
 # Create a subheader
 st.subheader("This is My Selection")
 
-# Create three select boxes
-selected_option_1 = st.selectbox("Select Option 1", ["Option 1", "Option 2", "Option 3"])
-selected_option_2 = st.selectbox("Select Option 2", ["Option A", "Option B", "Option C"])
-selected_option_3 = st.selectbox("Select Option 3", ["Choice X", "Choice Y", "Choice Z"])
+# Create a list to store select boxes
+select_boxes = []
+
+# Function to create a new select box
+def create_select_box():
+    if len(select_boxes) < 10:
+        new_select_box = st.selectbox(f"Select Option {len(select_boxes) + 1}", ["Option A", "Option B", "Option C"])
+        select_boxes.append(new_select_box)
+    else:
+        st.warning("You've reached the maximum limit of 10 select boxes.")
+
+# Create three initial select boxes
+for _ in range(3):
+    create_select_box()
+
+# Button to add a new select box
+if st.button("Add Another Select Box"):
+    create_select_box()
+
+# Display the select boxes
+for i, select_box in enumerate(select_boxes):
+    st.write(f"Option {i + 1}: {select_box}")
