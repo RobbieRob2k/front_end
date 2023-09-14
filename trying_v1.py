58010758bbb487fed28de7db771bedeec5307aaa
RobbieRob2k <139776035+RobbieRob2k@users.noreply.github.com>
2023-09-14 15:20:48 -0300

    Update trying_v1.py

--- trying_v1.py
+++ trying_v1.py
@@ -13,10 +13,6 @@ select_boxes = []
 for i in range(5):
     select_boxes.append(st.selectbox(f"Select Option {i + 1}", ["Option A", "Option B", "Option C"]))
 
-# Display the select boxes
-for i, select_box in enumerate(select_boxes):
-    st.write(f"Option {i + 1}: {select_box}")
-
 # URL to the image hosted on GitHub
 background_image_url = "https://github.com/RobbieRob2k/front_end/blob/main/wallpaper.jpg?raw=true"
 
