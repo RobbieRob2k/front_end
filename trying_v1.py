e82d912cf1cc62ada96489bdcdb12fda03ee8130
RobbieRob2k <139776035+RobbieRob2k@users.noreply.github.com>
2023-09-14 15:46:16 -0300

    Update trying_v1.py

--- trying_v1.py
+++ trying_v1.py
@@ -3,14 +3,19 @@ import time
 
 # Function to mimic a 30-second loading process
 def simulate_loading():
+    st.write("Loading in progress...")
+    progress_bar = st.progress(0)
+
     # Number of steps for the progress bar
     num_steps = 100
     # Time interval (in seconds) for each step
     step_interval = 30 / num_steps
 
     for i in range(num_steps + 1):
-        st.progress(i)
+        progress_bar.progress(i)
         time.sleep(step_interval)
+
+    st.success("Loading Complete!")
 
 # Run the app
 if __name__ == "__main__":
@@ -18,6 +23,4 @@ if __name__ == "__main__":
 
     # Button to start the loading process
     if st.button("Start Loading"):
-        st.text("Loading in progress...")
         simulate_loading()
-        st.success("Loading Complete!")
