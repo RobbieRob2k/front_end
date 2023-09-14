fe5483dc623fd1f4bfd8c7837c5f63d5667212a0
RobbieRob2k <139776035+RobbieRob2k@users.noreply.github.com>
2023-09-14 15:39:39 -0300

    Update trying_v1.py

--- trying_v1.py
+++ trying_v1.py
@@ -1,40 +1,33 @@
-import streamlit as st
+mport streamlit as st
 import time
 
-# Function to simulate loading UI
-def load_ui():
-    for i in range(101):
-        time.sleep(0.02)  # Simulate a delay
-        st.progress(i)
-    st.text("UI Loaded")
+# Function to mimic a 30-second loading process
+def simulate_loading():
+    # Number of steps for the progress bar
+    num_steps = 100
+    # Time interval (in seconds) for each step
+    step_interval = 30 / num_steps
 
-# Function to simulate API request
-def make_api_request():
-    for i in range(101):
-        time.sleep(0.02)  # Simulate a delay
+    for i in range(num_steps + 1):
         st.progress(i)
-    st.text("API Request Completed")
-
-# Function to simulate model processing
-def process_data_with_model():
-    for i in range(101):
-        time.sleep(0.02)  # Simulate a delay
-        st.progress(i)
-    st.text("Model Processing Completed")
+        time.sleep(step_interval)
 
 # Run the app
 if __name__ == "__main__":
-    st.title("Loading Progress Example")
+    st.title("Simulated Loading")
 
-    # Button to start the process
-    if st.button("Start Process"):
-        st.text("Loading UI...")
-        load_ui()
+    # Button to start the loading process
+    if st.button("Start Loading"):
+        st.text("Loading in progress...")
+        simulate_loading()
+        st.success("Loading Complete!")
+In this example, the simulate_loading function gradually updates the progress bar by looping through 100 steps over a 30-second duration. It introduces a delay between each step to create the effect of a slow loading process.
 
-        st.text("Making API Request...")
-        make_api_request()
+When you click the "Start Loading" button, you'll see the progress bar slowly fill over the course of 30 seconds, simulating a loading process. This approach allows you to mimic the appearance of a real loading time while actually controlling the duration programmatically.
 
-        st.text("Processing Data with Model...")
-        process_data_with_model()
+Feel free to adjust the num_steps and step_interval variables to fine-tune the speed and duration of the progress bar as needed for your application.
 
-        st.success("All Processes Completed!")
+
+
+
+
