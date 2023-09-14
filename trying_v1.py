2a32882a285421b553effc1fbbac29ea106170f3
RobbieRob2k <139776035+RobbieRob2k@users.noreply.github.com>
2023-09-14 15:05:46 -0300

    Update trying_v1.py

--- trying_v1.py
+++ trying_v1.py
@@ -12,18 +12,14 @@ select_boxes = []
 # Function to create a new select box
 def create_select_box():
     if len(select_boxes) < 10:
-        new_select_box = st.selectbox(f"Select Option {len(select_boxes) + 1}", ["Option A", "Option B", "Option C"])
-        select_boxes.append(new_select_box)
+        select_boxes.append(st.selectbox(f"Select Option {len(select_boxes) + 1}", ["Option A", "Option B", "Option C"]))
 
 # Create three initial select boxes
 for _ in range(3):
     create_select_box()
 
 # Button to add a new select box
-add_button = st.button("Add Another Select Box")
-
-# Create a new select box when the button is clicked
-if add_button:
+if st.button("Add Another Select Box"):
     create_select_box()
 
 # Display the select boxes
