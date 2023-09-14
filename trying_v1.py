e154a1eeb8ad8d4d865cdb8013ae226a00a7ec5f
RobbieRob2k <139776035+RobbieRob2k@users.noreply.github.com>
2023-09-14 15:10:28 -0300

    Update trying_v1.py

--- trying_v1.py
+++ trying_v1.py
@@ -9,15 +9,23 @@ st.subheader("This is My Selection")
 # Create a list to store select boxes
 select_boxes = []
 
-# Function to create a new select box
-def create_select_box():
-    if len(select_boxes) < 10:
-        select_boxes.append(st.selectbox(f"Select Option {len(select_boxes) + 1}", ["Option A", "Option B", "Option C"]))
-
-# Button to add a new select box
-if st.button("Add Another Select Box"):
-    create_select_box()
+# Create five initial select boxes
+for i in range(5):
+    select_boxes.append(st.selectbox(f"Select Option {i + 1}", ["Option A", "Option B", "Option C"]))
 
 # Display the select boxes
 for i, select_box in enumerate(select_boxes):
     st.write(f"Option {i + 1}: {select_box}")
+
+# Add a background image
+st.markdown(
+    """
+    <style>
+    body {
+        background-image: url("wallpaper.jpg");
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
