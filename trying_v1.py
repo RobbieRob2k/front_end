2aa463cbda5a8bf9b575c86d45f4efbf3e656797
RobbieRob2k <139776035+RobbieRob2k@users.noreply.github.com>
2023-09-14 15:16:47 -0300

    Update trying_v1.py

--- trying_v1.py
+++ trying_v1.py
@@ -18,7 +18,7 @@ for i, select_box in enumerate(select_boxes):
     st.write(f"Option {i + 1}: {select_box}")
 
 # URL to the image hosted on GitHub
-background_image_url = "https://raw.githubusercontent.com/RobbieRob2k/front_end/main/wallpaper.jpg"
+background_image_url = "https://github.com/RobbieRob2k/front_end/blob/main/wallpaper.jpg?raw=true"
 
 # Add a background image
 st.markdown(
