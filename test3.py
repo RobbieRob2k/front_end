50342c5e577502804b71431b2626aa6bc52b507a
RobbieRob2k <139776035+RobbieRob2k@users.noreply.github.com>
2023-09-14 10:13:04 -0300

    test4.py

--- test3.py
+++ test3.py
@@ -1,41 +1,39 @@
 import streamlit as st
 import requests
 
-# API endpoints
-# recommendations_endpoint = "https://api.example.com/recommendations"  
+# API endpoint 
+recommendations_endpoint = "http://localhost:8000/predict"
 
-# Read the list of movies from the text file
+# reads list of movies saved in this text file, needs to be updated once new movies added; note: ASIN formatting
 with open("movies2.txt", "r", encoding="cp1252") as file:
     movies_list = [line.strip() for line in file]
 
-# Add subheaders to indicate the purpose of the selectboxes
 st.subheader("Select Your Top 5 Best Movies:")
-
 selected_movies_best = [st.selectbox(f"Select Movie (Best) {i+1}", movies_list, key=f"best_movie_{i}") for i in range(5)]
-st.subheader("Select Your Top 5 Least Liked Movies:")
 
+st.subheader("Select Your Top 5 Least Liked Movies:")
 selected_movies_least_liked = [st.selectbox(f"Select Movie (Least Liked) {i+1}", movies_list, key=f"least_liked_movie_{i}") for i in range(5)]
 
-# Combine the selected movies into a single list
+# combining most liked and disliked into single list for API
 selected_movies = selected_movies_best + selected_movies_least_liked
 
-# Function to get movie recommendations and top genres from the API
+# function to receive movie recommendations and top genres from API
 def get_recommendations_and_genres(selected_movies_fav, selected_movies_dislike):
     try:
-        # Create a JSON payload with selected movies
+        # JSON payload with selected movies
         payload = {
-            "favorite_movies": selected_movies_fav,
+            "liked_movies": selected_movies_fav,
             "disliked_movies": selected_movies_dislike
         }
 
         # Make a POST request to the recommendations endpoint
-        response = requests.post(recommendations_endpoint, json=payload)
+        response = requests.get(recommendations_endpoint, params=payload)
 
-        # Check if the request was successful
+        # check if the request was successful
         if response.status_code == 200:
             data = response.json()
-            recommendations = data.get("recommendations", [])
-            top_genres = data.get("top_genres", [])
+            recommendations = data.get("Suggested Movies", [])
+            top_genres = data.get("Top Genres", [])
             return recommendations, top_genres
         else:
             st.error(f"Error fetching recommendations: {response.status_code}")
@@ -45,21 +43,22 @@ def get_recommendations_and_genres(selected_movies_fav, selected_movies_dislike)
         st.error(f"API request error: {e}")
         return [], []
 
-# Create a button to fetch movie recommendations and top genres
-if st.button("Get Recommendations"):
-    recommendations, top_genres = get_recommendations_and_genres(selected_movies_fav, selected_movies_dislike)
+# button on UI to get recommendations
+if st.button("Get My Movie Recommendations!"):
+    recommendations, top_genres = get_recommendations_and_genres(selected_movies_best, selected_movies_least_liked)
 
-    # Display recommendations to the user
-    st.subheader("Recommended Movies:")
-    for i, movie in enumerate(recommendations):
-        st.write(f"{i+1}. {movie}")
+    if recommendations:
+        st.subheader("Your Top 10 Recommended Movies:")
+        for i, movie in enumerate(recommendations):
+            st.write(f"{i+1}. {movie}")
+    else:
+        st.info("No recommendations available based on your selections.")
 
     # Display top genres
     st.subheader("Top Genres Based on Your Selections:")
     for i, genre in enumerate(top_genres):
         st.write(f"{i+1}. {genre}")
 
-print("Run the app")
 if __name__ == "__main__":
     st.set_page_config(page_title="Your Personalized Movie Recommendations")
     st.write("Instructions: Select your top 5 favorite and top 5 least liked movies, and click 'Get Recommendations' to view movie recommendations and top genres.")
