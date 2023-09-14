b998e70663c562658f7efb4e9a949dd17909bacd
RobbieRob2k <139776035+RobbieRob2k@users.noreply.github.com>
2023-09-14 15:50:11 -0300

    Update trying_v1.py

--- trying_v1.py
+++ trying_v1.py
@@ -1,26 +1,69 @@
 import streamlit as st
-import time
+import requests
 
-# Function to mimic a 30-second loading process
-def simulate_loading():
-    st.write("Loading in progress...")
-    progress_bar = st.progress(0)
+st.title("Get Your AI-Powered Movie Recommendations 🎬🤖🍿", anchor="center")
 
-    # Number of steps for the progress bar
-    num_steps = 100
-    # Time interval (in seconds) for each step
-    step_interval = 30 / num_steps
+# API endpoint 
+recommendations_endpoint = "http://localhost:8000/predict"
 
-    for i in range(num_steps + 1):
-        progress_bar.progress(i)
-        time.sleep(step_interval)
+# reads list of movies saved in this text file, needs to be updated once new movies added; note: ASIN formatting
+with open("filmaholic/interface/movies2.txt", "r", encoding="cp1252") as file:
+    movies_list = [line.strip() for line in file]
 
-    st.success("Loading Complete!")
+st.subheader("Select Your Favorite Movies:")
+selected_movies_best = [st.selectbox(f"Select Favorite Movie {i+1}", movies_list, key=f"best_movie_{i}") for i in range(5)]
 
-# Run the app
+st.subheader("Select Your Least Favorite Movies:")
+selected_movies_least_liked = [st.selectbox(f"Select Least Favorite Movie {i+1}", movies_list, key=f"least_liked_movie_{i}") for i in range(5)]
+
+# combining most liked and disliked into a single list for API
+selected_movies = selected_movies_best + selected_movies_least_liked
+
+# function to receive movie recommendations and top genres from API
+def get_recommendations_and_genres(selected_movies_fav, selected_movies_dislike):
+    try:
+        # JSON payload with selected movies
+        payload = {
+            "liked_movies": selected_movies_fav,
+            "disliked_movies": selected_movies_dislike
+        }
+
+        # Make a POST request to the recommendations endpoint
+        response = requests.get(recommendations_endpoint, params=payload)
+
+        # check if the request was successful
+        # add genre to return term once set up
+        if response.status_code == 200:
+            data = response.json()
+            recommendations = data.get("Suggested Movies", [])
+            # top_genres = data.get("Top Genres", [])
+            return recommendations
+        else:
+            st.error(f"Error fetching recommendations: {response.status_code}")
+            return [], []
+
+    except requests.exceptions.RequestException as e:
+        st.error(f"API request error: {e}")
+        return [], []
+
+# button on UI to get recommendations
+# need to add genres to st.button once we can load them
+if st.button("Get My Movie Recommendations!"):
+    with st.spinner("Processing your request..."):
+        recommendations = get_recommendations_and_genres(selected_movies_best, selected_movies_least_liked)
+
+    if recommendations:
+        st.subheader("Your Top 10 Recommended Movies:")
+        for i, movie in enumerate(recommendations):
+            st.write(f"{i+1}. {movie}")
+    else:
+        st.info("No recommendations available based on your selections.")
+
+    # Display top genres
+    # st.subheader("Top Genres Based on Your Selections:")
+    # for i, genre in enumerate(top_genres):
+    #    st.write(f"{i+1}. {genre}")
+
 if __name__ == "__main__":
-    st.title("Simulated Loading")
-
-    # Button to start the loading process
-    if st.button("Start Loading"):
-        simulate_loading()
+    st.set_page_config(page_title="Your Personalized Movie Recommendations")
+    st.write("Instructions: Select your top 5 favorite and top 5 least liked movies, and click 'Get Recommendations' to view movie recommendations and top genres.")
