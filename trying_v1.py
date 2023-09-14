47a0b8677fb32fdb4882fb43bc550ccea31fc881
RobbieRob2k <139776035+RobbieRob2k@users.noreply.github.com>
2023-09-14 15:25:34 -0300

    Update trying_v1.py

--- trying_v1.py
+++ trying_v1.py
@@ -1,30 +1,63 @@
 import streamlit as st
+import requests
 
-# Set the title of the web page
-st.title("This is a Test Website")
+st.title("Get Your AI-Powered Movie Recommendations", anchor="center")
 
-# Create a subheader
-st.subheader("This is My Selection")
+# API endpoint 
+recommendations_endpoint = "http://localhost:8000/predict"
 
-# Create a list to store select boxes
-select_boxes = []
+# reads list of movies saved in this text file, needs to be updated once new movies added; note: ASIN formatting
+with open("filmaholic/interface/movies2.txt", "r", encoding="cp1252") as file:
+    movies_list = [line.strip() for line in file]
 
-# Create five initial select boxes
-for i in range(5):
-    select_boxes.append(st.selectbox(f"Select Option {i + 1}", ["Option A", "Option B", "Option C"]))
+st.subheader("Select Your Favorite Movies:")
+selected_movies_best = [st.selectbox(f"Select Movie (Best) {i+1}", movies_list, key=f"best_movie_{i}") for i in range(5)]
 
-# URL to the image hosted on GitHub
-background_image_url = "https://github.com/RobbieRob2k/front_end/blob/main/wallpaper.jpg?raw=true"
+st.subheader("Select Your Least Favorite Movies:")
+selected_movies_least_liked = [st.selectbox(f"Select Movie (Least Liked) {i+1}", movies_list, key=f"least_liked_movie_{i}") for i in range(5)]
 
-# Add a background image
-st.markdown(
-    f"""
-    <style>
-    body {{
-        background-image: url("{background_image_url}");
-        background-size: cover;
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# combining most liked and disliked into single list for API
+selected_movies = selected_movies_best + selected_movies_least_liked
+
+# function to receive movie recommendations and top genres from API
+def get_recommendations_and_genres(selected_movies_fav, selected_movies_dislike):
+    try:
+        # JSON payload with selected movies
+        payload = {
+            "liked_movies": selected_movies_fav,
+            "disliked_movies": selected_movies_dislike
+        }
+
+        # Make a POST request to the recommendations endpoint
+        response = requests.get(recommendations_endpoint, params=payload)
+
+        # check if the request was successful
+        if response.status_code == 200:
+            data = response.json()
+            recommendations = data.get("Suggested Movies", [])
+            # top_genres = data.get("Top Genres", [])
+            return recommendations, top_genres
+        else:
+            st.error(f"Error fetching recommendations: {response.status_code}")
+            return [], []
+
+    except requests.exceptions.RequestException as e:
+        st.error(f"API request error: {e}")
+        return [], []
+
+# button on UI to get recommendations
+# need to add genres to st.button once we can load them
+if st.button("Get My Movie Recommendations!"):
+    recommendations = get_recommendations_and_genres(selected_movies_best, selected_movies_least_liked)
+
+    if recommendations:
+        st.subheader("Your Top 10 Recommended Movies:")
+        for i, movie in enumerate(recommendations):
+            st.write(f"{i+1}. {movie}")
+    else:
+        st.info("No recommendations available based on your selections.")
+
+    # Display top genres
+    # st.subheader("Top Genres Based on Your Selections:")
+    # for i, genre in enumerate(top_genres):
+    #    st.write(f"{i+1}. {genre}")
